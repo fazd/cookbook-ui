3814e5d044c9b457722c7562ab4bd8fe5b920de9
fabio <fabiozapata980729@gmail.com>
2022-02-16 09:58:06 -0500

    set new size to all cards

--- src/components/recipe-list.jsx
+++ src/components/recipe-list.jsx
@@ -78,8 +78,8 @@ const RecipeList = (props) => {
           <div
             className='justify-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'
           >
-            <div className='relative w-50 my-6 min-w-xl  mx-auto max-w-xl'>
-              <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+            <div className='relative my-6 w-50 min-w-xl  mx-auto max-w-xl'>
+              <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-96	 bg-white outline-none focus:outline-none'>
                 <div className='flex justify-center p-5 border-b border-solid border-blueGray-200 rounded-t'>
                   <h3 className='text-3xl font-bold justify-center'>
                     {selected.title}
