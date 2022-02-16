76b6cb71d529db4a70c17f6b3e62d3c4738f6025
fabio <fabiozapata980729@gmail.com>
2022-02-15 20:33:30 -0500

    add message to registers

--- src/screens/register/register.jsx
+++ src/screens/register/register.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { ToastContainer } from 'react-toastify';
+import { errorNotify, successNotify } from '../../components/alert';
 import { register } from '../../services/auth';
 
 const Register = () => {
+
+  const history = useHistory();
 
   const signUp = async (event) => {
     const form = event.target.form;
@@ -12,14 +17,19 @@ const Register = () => {
     const name = form.name.value;
     const password2 = form.password2.value;
 
-
     if (password === password2) {
-      const response = await register({ email, password, name });
-      console.log('response', response);
+      try {
+        await register({ email, password, name });
+        successNotify('You have successfully registered.', 1000);
+        setTimeout(() => {
+          history.push('/login');
+        }, 2000);
+      } catch (error) {
+        errorNotify('This email has already an account');
+      }
     } else {
-      console.log('failed');
+      errorNotify('The passwords do not match.');
     }
-
   }
 
 
@@ -56,7 +66,6 @@ const Register = () => {
           <div>
             <button type="button" onClick={signUp} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                {/* <!-- Heroicon name: solid/lock-closed --> */}
                 <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                 </svg>
@@ -65,6 +74,7 @@ const Register = () => {
             </button>
           </div>
         </form>
+        <ToastContainer />
       </div >
     </div >
   )
